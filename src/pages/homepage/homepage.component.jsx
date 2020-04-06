04318738b1d24a5501169bdbe0825542fdb60a09
Aavash <aavashkhatri95@gmail.com>
2020-04-06 21:14:21 +0545

    Structural refactor - Separate modules - Add new homepage modules

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,48 +1,13 @@
 import React from "react";
 
-import '../../homepage.style.scss'
+import Directory from "../../components/directory/directory.component";
+import './homepage.style.scss'
 
 const HomePage = () => {
     return (
         <div className='homepage'>
-            <div className='directory-menu'>
-
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>HATS</h1>
-                        <span className='subtitle'>Shop Now</span>
-                    </div>
-                </div>
-
-                <div className='menu-item'>
-                    <div className='content     '>
-                        <h1 className='title'>JACKETS</h1>
-                        <span className='subtitle'>Shop Now</span>
-                    </div>
-                </div>
-
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>SNEAKERS</h1>
-                        <span className='subtitle'>Shop Now</span>
-                    </div>
-                </div>
-
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>WOMEN</h1>
-                        <span className='subtitle'>Shop Now</span>
-                    </div>
-                </div>
-
-                <div className='menu-item'>
-                    <div className='content'>
-                        <h1 className='title'>MEN</h1>
-                        <span className='subtitle'>Shop Now</span>
-                    </div>
-                </div>
-            </div>
+            <Directory/>
         </div>
-)};
+    )};
 
 export default HomePage;
